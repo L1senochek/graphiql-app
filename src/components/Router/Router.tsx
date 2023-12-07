4d4565f15441c129ae68ea3e1ba406f729306b8a
L1senochek <lisenochek96@gmail.com>
2023-12-08 00:35:14 +0300

    feat: add sign-up route

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -7,12 +7,14 @@ import {
 import ErrorMessage from '@/components/ErrorMessage/ErrorMessage';
 import NotFound from '@/components/NotFound/NotFound';
 import Welcome from '@/pages/Welcome/Welcome';
+import SignUp from '@/pages/SignUp/SignUp';
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Layout />} errorElement={<ErrorMessage />}>
         <Route path={'/welcome'} element={<Welcome />} />
+        <Route path={'/sign-up'} element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </>
