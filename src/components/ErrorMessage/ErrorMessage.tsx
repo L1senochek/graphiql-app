dfdb6ce272c831825503d29fad36c9b4cf46a2b9
L1senochek <lisenochek96@gmail.com>
2023-12-07 23:45:15 +0300

    feat: add ErrorMessage styles

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,14 +1,17 @@
 import { FC } from 'react';
 import { Link, useRouteError } from 'react-router-dom';
+import styles from './error-message.module.scss';
 
 const ErrorMessage: FC = (): JSX.Element => {
   const error = useRouteError() as Error;
 
   return (
-    <div>
-      <h2>Error message:</h2>
-      <h3>{error.message}</h3>
-      <Link to={'/'}>Go to the Home</Link>
+    <div className={styles['error-message']}>
+      <h2 className={styles['error-message__title']}>Error message:</h2>
+      <h3 className={styles['error-message__message']}>{error.message}</h3>
+      <Link className={styles['error-message__btn btn']} to={'/'}>
+        Go to the Home
+      </Link>
     </div>
   );
 };
