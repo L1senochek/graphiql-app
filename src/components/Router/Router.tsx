de2404fe8c657c5fb3508657636cd8b36e1c750a
L1senochek <lisenochek96@gmail.com>
2023-12-08 00:37:04 +0300

    feat: add graphi-ql router

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -8,6 +8,7 @@ import ErrorMessage from '@/components/ErrorMessage/ErrorMessage';
 import NotFound from '@/components/NotFound/NotFound';
 import Welcome from '@/pages/Welcome/Welcome';
 import SignUp from '@/pages/SignUp/SignUp';
+import GraphiQL from '@/pages/GraphiQL/GraphiQL';
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
@@ -15,6 +16,7 @@ const Router = createBrowserRouter(
       <Route path="/" element={<Layout />} errorElement={<ErrorMessage />}>
         <Route path={'/welcome'} element={<Welcome />} />
         <Route path={'/sign-up'} element={<SignUp />} />
+        <Route path={'/graphi-ql'} element={<GraphiQL />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </>
---
+++ src/pages/GraphiQL/GraphiQL.tsx
@@ -0,0 +1,5 @@
+const GraphiQL: React.FC = (): JSX.Element => {
+  return <div>GraphiQL</div>;
+};
+
+export default GraphiQL;
