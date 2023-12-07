7bd3e76748c6c048abd36603163e48572b0c7aa3
L1senochek <lisenochek96@gmail.com>
2023-12-07 23:56:50 +0300

    feat: add NotFound styles

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import styles from './not-found.module.scss';
 
 const NotFound: FC = (): JSX.Element => {
   return (
-    <div>
-      <h2>Page not found!</h2>
-      <h2>404</h2>
-      <Link to={'/'}>Go to the Home</Link>
+    <div className={styles['not-found']}>
+      <h2 className={styles['not-found__title']}>Page not found!</h2>
+      <h2 className={styles['not-found__message']}>404</h2>
+      <Link className={styles['not-found__btn btn']} to={'/'}>
+        Go to the Home
+      </Link>
     </div>
   );
 };
