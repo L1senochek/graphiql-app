656ec782ab138ad6f85db41f469979522e3e76e7
akatimer <akatimer@gmail.com>
2023-12-07 15:59:42 +0200

    feat: setup test & add simple test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import vitest from 'vitest';
 
-// https://vitejs.dev/config/
+const testConfig: vitest.InlineConfig = {
+  coverage: {
+    provider: 'istanbul',
+  },
+  globals: true,
+  environment: 'jsdom',
+  setupFiles: './src/utils/tests/setup.ts',
+};
+
 export default defineConfig({
   plugins: [react()],
-})
+  test: testConfig,
+});
