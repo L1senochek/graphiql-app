5406cee6d30e36927ac7ed4c52fb3d18d10d8758
L1senochek <lisenochek96@gmail.com>
2023-12-08 14:22:33 +0300

    feat: update styles NotFound & ErrorMessage

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,13 +1,12 @@
-import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './not-found.module.scss';
 
-const NotFound: FC = (): JSX.Element => {
+const NotFound: React.FC = (): JSX.Element => {
   return (
     <div className={styles['not-found']}>
       <h2 className={styles['not-found__title']}>Page not found!</h2>
       <h2 className={styles['not-found__message']}>404</h2>
-      <Link className={styles['not-found__btn btn']} to={'/'}>
+      <Link className={`${styles['not-found__btn']} btn`} to={'/'}>
         Go to the Home
       </Link>
     </div>
