a09f1df657bbd5d8c99362999e9e6fef28b8ec36
L1senochek <lisenochek96@gmail.com>
2023-12-09 17:46:34 +0300

    feat: add links & btn in header

--- src/components/Header/HeaderSticky.tsx
+++ src/components/Header/HeaderSticky.tsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
 import styles from './header.module.scss';
+import { INITIAL_PATH } from '@/utils/const/const';
 
 const Header: React.FC = (): JSX.Element => {
   return (
     <>
       <header className={styles.header}>
-        <Link to={'/'}>header</Link>
+        <div className={styles['header__left-side']}>
+          <Link to={INITIAL_PATH}>Welcome</Link>
+        </div>
+        <div className={styles['header__right-side']}>
+          <button>Sign Out</button>
+          <div>settings</div>
+        </div>
       </header>
     </>
   );
