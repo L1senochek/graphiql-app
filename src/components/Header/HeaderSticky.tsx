dae97115780e0615ac89187dfa7f74344952ef57
L1senochek <lisenochek96@gmail.com>
2023-12-09 19:57:13 +0300

    feat: add styles & header sticky

--- src/components/Header/HeaderSticky.tsx
+++ src/components/Header/HeaderSticky.tsx
@@ -1,18 +1,43 @@
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import styles from './header.module.scss';
 import { INITIAL_PATH } from '@/utils/const/const';
+import styles from './header.module.scss';
 import IconSettings from '../IconSettings/IconSettings';
 
 const Header: React.FC = (): JSX.Element => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = (): void => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.onscroll = handleScroll;
+
+    return (): void => {
+      window.onscroll = null;
+    };
+  }, []);
+
   return (
     <>
-      <header className={styles.header}>
-        <div className={styles['header__left-side']}>
-          <Link to={INITIAL_PATH}>Welcome</Link>
-        </div>
-        <div className={styles['header__right-side']}>
-          <button className={`${styles['header__btn']} btn`}>Sign Out</button>
-          <IconSettings />
+      <header
+        className={`${styles.header} ${isSticky ? styles['sticky'] : ''}`}
+      >
+        <div className={styles['header__wrapper']}>
+          <div className={styles['header__left-side']}>
+            <Link to={INITIAL_PATH}>
+              <h2>Welcome</h2>
+            </Link>
+          </div>
+          <div className={styles['header__right-side']}>
+            <button className={`${styles['header__btn']} btn`}>Sign Out</button>
+            <IconSettings />
+          </div>
         </div>
       </header>
     </>
