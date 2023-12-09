665592e407b96cfabecbf1e666b033c2055e0f12
L1senochek <lisenochek96@gmail.com>
2023-12-09 21:06:46 +0300

    feat: add Btn component & update styles

---
+++ src/components/Btn/Btn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './btn.module.scss';
+import IBtn from '@/model/components/Btn/Btn';
+
+const Btn: React.FC<IBtn> = ({ className, ...props }): JSX.Element => {
+  return (
+    <button className={`${styles.btn} ${className}`} {...props}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Btn;
--- src/components/Header/HeaderSticky.tsx
+++ src/components/Header/HeaderSticky.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { INITIAL_PATH } from '@/utils/const/const';
 import styles from './header.module.scss';
-import IconSettings from '../IconSettings/IconSettings';
+import IconSettings from '@/components/IconSettings/IconSettings';
+import Btn from '@/components/Btn/Btn';
 
 const Header: React.FC = (): JSX.Element => {
   const [isSticky, setIsSticky] = useState(false);
@@ -36,6 +37,7 @@ const Header: React.FC = (): JSX.Element => {
           </div>
           <div className={styles['header__right-side']}>
             <button className={`${styles['header__btn']} btn`}>Sign Out</button>
+            <Btn className={styles['header__btn']}>Sign Out</Btn>
             <IconSettings />
           </div>
         </div>
