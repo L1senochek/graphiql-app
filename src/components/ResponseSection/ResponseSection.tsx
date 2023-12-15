768cc7f692d42e9bf3a4ed78a34ec81ccb2da138
l1senochek <lisenochek96@gmail.com>
2023-12-15 16:35:59 +0300

    feat: update styles GraphiQL page

--- src/components/ResponseSection/ResponseSection.tsx
+++ src/components/ResponseSection/ResponseSection.tsx
@@ -1,9 +1,13 @@
 import styles from './response-section.module.scss';
+import resJson from './resTest.json';
 
 const ResponseSection: React.FC = (): JSX.Element => {
+  const formattedJson = JSON.stringify(resJson, null, 8);
+
   return (
     <div className={styles['response-section']}>
       <>ResponseSection</>
+      <pre className={styles['response-section__pre']}>{formattedJson}</pre>
     </div>
   );
 };
