fda9f6ea97763e4dc09c7d84bf0a9387384da414
l1senochek <lisenochek96@gmail.com>
2023-12-18 14:52:30 +0300

    feat: add IconDocs & styles

--- src/components/TopSection/TopSection.tsx
+++ src/components/TopSection/TopSection.tsx
@@ -1,9 +1,10 @@
 import HeadersEditor from '@/components/HeadersEditor/HeadersEditor';
 import Btn from '@/components/Btn/Btn';
-import styles from './top-secton.module.scss';
+import styles from './top-section.module.scss';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { RootState } from '@/store/store';
 import { setClickDocBtn } from '@/store/slices/documentationSlice';
+import IconDocs from '@/components/IconDocs/IconDocs';
 
 const TopSection: React.FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
@@ -11,9 +12,14 @@ const TopSection: React.FC = (): JSX.Element => {
     (state: RootState) => state.documentationSlice.clickDocBtn
   );
   return (
-    <div className={styles['top-secton']}>
-      <Btn onClick={() => dispatch(setClickDocBtn(!isClickDocBtn))}>Doc</Btn>
-      <h2 className={styles['top-secton__title']}>GraphiQL</h2>
+    <div className={styles['top-section']}>
+      <Btn
+        className={styles['top-section__btn']}
+        onClick={() => dispatch(setClickDocBtn(!isClickDocBtn))}
+      >
+        <IconDocs />
+      </Btn>
+      <h2 className={styles['top-section__title']}>GraphiQL</h2>
       <HeadersEditor />
     </div>
   );
