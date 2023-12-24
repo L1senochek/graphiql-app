66cf325a80b3173a1eda2d3330b67bca0a607fc9
akatimer <akatimer@gmail.com>
2023-12-24 19:55:24 +0200

    feat: add isAuth function

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -2,12 +2,15 @@ import Link from '@/components/Welcome/Links/Link';
 import Team from '@/components/Welcome/Team/Team';
 import contentJson from '@/utils/jsons/WelcomeContent/WelcomeContent.json';
 import styles from './welcome.module.scss';
-import { useAppSelector } from '@/store/hooks';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { RootState } from '@/store/store';
+import isAuthFirebase from '@/utils/auth/isAuth';
 
 const Welcome = () => {
   const isEn = useAppSelector((state: RootState) => state.languageSlice.eng);
   const content = isEn ? contentJson.eng : contentJson.ru;
+  const dispatch = useAppDispatch();
+  isAuthFirebase(dispatch);
 
   return (
     <div className={styles.welcome}>
