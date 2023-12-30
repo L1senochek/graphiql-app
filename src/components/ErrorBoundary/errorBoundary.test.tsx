3abe219dc2df6fca01b367d2828305761c6c4bfe
dmitrij <dmitrijkrymskij6@gmail.com>
2023-12-31 00:48:23 +0400

    fix: test for error boundary

--- src/components/ErrorBoundary/errorBoundary.test.tsx
+++ src/components/ErrorBoundary/errorBoundary.test.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { describe, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { Provider } from 'react-redux';
 import getStore from '@/store/store';
 import ErrorBoundary from './ErrorBoundary';
@@ -15,6 +15,10 @@ vi.mock('react-router-dom', async () => {
 });
 
 describe('ErrorBoundary Component', () => {
+  beforeEach(() => {
+    vi.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
   it('renders ErrorBoundary component', () => {
     const store = getStore();
     render(
@@ -26,5 +30,39 @@ describe('ErrorBoundary Component', () => {
         </MemoryRouter>
       </Provider>
     );
+  });
+  it('displays an error message', () => {
+    const ProblemChild = () => {
+      throw new Error('Error problem child');
+      return <div></div>;
+    };
+    const store = getStore();
+    const spy = vi.spyOn(global.console, 'error').mockImplementation(() => {});
+    const { getByText } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ErrorBoundary>
+            <ProblemChild />
+          </ErrorBoundary>
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(getByText(/something went wrong/i)).toBeInTheDocument();
+    expect(getByText('Back')).toBeInTheDocument();
+    spy.mockRestore();
+  });
+  it('does not display an error message', () => {
+    const store = getStore();
+    const { queryByText } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ErrorBoundary>
+            <IconRun />
+          </ErrorBoundary>
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(queryByText(/something went wrong/i)).not.toBeInTheDocument();
   });
 });
