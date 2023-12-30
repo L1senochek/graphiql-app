b8726738438c8f9454e0a304be42ffb1cc32981b
dmitrij <dmitrijkrymskij6@gmail.com>
2023-12-31 01:50:52 +0400

    fix: test for settings

--- src/components/Settings/settings.test.tsx
+++ src/components/Settings/settings.test.tsx
@@ -1,6 +1,6 @@
 import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-import { describe, it, vi } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it, vi } from 'vitest';
 import { Provider } from 'react-redux';
 import getStore from '@/store/store';
 import Settings from './Settings';
@@ -23,5 +23,24 @@ describe('Settings Component', () => {
         </MemoryRouter>
       </Provider>
     );
+  });
+  it('toggles the menu open/closed when clicking the settings button', () => {
+    const store = getStore();
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Settings />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    const settingsBtn = screen.getByRole('switch');
+
+    expect(screen.getByRole('switch')).not.toHaveClass('_open');
+
+    fireEvent.click(settingsBtn);
+    fireEvent.click(settingsBtn);
+
+    expect(screen.getByRole('switch')).not.toHaveClass('open');
   });
 });
