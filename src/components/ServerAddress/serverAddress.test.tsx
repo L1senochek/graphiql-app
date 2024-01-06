9e7ba305e33abb87cf26bdd8c68a148d4524332b
dmitrij <dmitrijkrymskij6@gmail.com>
2024-01-06 21:03:34 +0400

    fix: test for server address

--- src/components/ServerAddress/serverAddress.test.tsx
+++ src/components/ServerAddress/serverAddress.test.tsx
@@ -5,15 +5,14 @@ import ServerAddress from './ServerAddress';
 import getStore from '@/store/store';
 import { Store, UnknownAction } from '@reduxjs/toolkit';
 import { setServerAddressInputValue } from '@/store/slices/serverAddressSlice';
+import { setDocLoading } from '@/store/slices/documentationSlice';
 
 describe('ServerAddress Component', () => {
   let store: Store<unknown, UnknownAction, object>;
-
   beforeEach(() => {
     store = getStore();
     store.dispatch = vi.fn();
   });
-
   afterEach(() => {
     cleanup();
   });
@@ -24,24 +23,28 @@ describe('ServerAddress Component', () => {
         <ServerAddress />
       </Provider>
     );
-
     const inputElement = screen.getByPlaceholderText('http://api-example.com');
     expect(inputElement).toBeInTheDocument();
   });
 
-  it('should dispatch an action on input change', () => {
+  it('should dispatch actions on button click after input change', () => {
     render(
       <Provider store={store}>
         <ServerAddress />
       </Provider>
     );
-
     const inputElement = screen.getByPlaceholderText('http://api-example.com');
+    const buttonElement = screen.getByRole('button');
+
     fireEvent.change(inputElement, { target: { value: 'http://new-api.com' } });
 
+    fireEvent.click(buttonElement);
+
     expect(store.dispatch).toHaveBeenCalled();
+
     expect(store.dispatch).toHaveBeenCalledWith(
       setServerAddressInputValue('http://new-api.com')
     );
+    expect(store.dispatch).toHaveBeenCalledWith(setDocLoading(true));
   });
 });
