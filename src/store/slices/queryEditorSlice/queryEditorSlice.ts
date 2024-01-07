e5a51a6c3fca20a124759096a25ce7bcb54c7311
L1senochek <lisenochek96@gmail.com>
2024-01-07 19:30:36 +0300

    feat: update styles

--- src/store/slices/queryEditorSlice/queryEditorSlice.ts
+++ src/store/slices/queryEditorSlice/queryEditorSlice.ts
@@ -32,7 +32,7 @@ const initialState: IQueryEditorState = {
 `,
   requestLineNumbers: [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27,
+    22, 23, 24, 25, 26, 27, 28,
   ],
   variablesCode: `{
   "locationId": "2",
